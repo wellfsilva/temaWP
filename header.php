78dac6566fe41a3a366c4eafd3209211b957320d
Wellington <designerwell@gmail.com>
2023-03-02 12:44:23 -0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,18 @@
+<html lang="pt-br">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <title> <!--  Título do site --> </title>
+     
+      <!-- Bootstrap CSS -->
+      
+      <!-- Google fonts -->
+     
+      <!-- Style CSS -->
+      <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+
+      <?php wp_head(); ?>
+      
+      <!-- Google analytics tag (gtag.js) -->
+  
+   </head>
